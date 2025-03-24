eb9a060f0a90da9dc9c364fde521e1d5cc9d3f5c
Sergei Korotaev <zergwsmt@gmail.com>
2025-03-24 21:47:39 +0300

    InitialCommit. Tasks 1 & 2

--- app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
+++ app/src/main/java/otus/gpb/homework/activities/EditProfileActivity.kt
@@ -1,11 +1,20 @@
 package otus.gpb.homework.activities
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class EditProfileActivity : AppCompatActivity() {
 
@@ -28,6 +37,12 @@ class EditProfileActivity : AppCompatActivity() {
                 }
             }
         }
+
+        findViewById<ImageView>(R.id.imageview_photo).apply {
+            setOnClickListener {
+                makeImage()
+            }
+        }
     }
 
     /**
@@ -40,5 +55,82 @@ class EditProfileActivity : AppCompatActivity() {
 
     private fun openSenderApp() {
         TODO("В качестве реализации метода отправьте неявный Intent чтобы поделиться профилем. В качестве extras передайте заполненные строки и картинку")
+    }
+
+    private fun makeImage() {
+        MaterialAlertDialogBuilder(this)
+            .setItems(
+                arrayOf(
+                    resources.getText(R.string.take_photo),
+                    resources.getText(R.string.select_photo)
+                ), ::onSelPhotoMethod
+            )
+            .show()
+    }
+
+    private fun onSelPhotoMethod(dialog : DialogInterface, which: Int) {
+        when (which) {
+            0 -> {
+                if (ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED) {
+                    takeShot()
+                }
+                else {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                        MaterialAlertDialogBuilder(this)
+                            .setMessage(resources.getString(R.string.access_camera_reason))
+                            .setPositiveButton(R.string.give_access, ::onGiveCameraAccess)
+                            .setNegativeButton(R.string.deny_access, null)
+                            .show()
+                    }
+                    else {
+                        permissionCamera.launch(Manifest.permission.CAMERA)
+                    }
+                }
+            }
+            else -> selectImage()
+        }
+    }
+
+    private fun showAccessCameraReason() {
+        MaterialAlertDialogBuilder(this)
+            .setMessage(resources.getString(R.string.access_camera_reason))
+            .setNeutralButton(R.string.go_access_settings, ::onGoAccessSettings)
+            .show()
+    }
+
+    private fun onGiveCameraAccess(dialog : DialogInterface, which: Int) {
+        permissionCamera.launch(Manifest.permission.CAMERA)
+    }
+
+    private val permissionCamera = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+        granted->when {
+            granted->takeShot()
+            !shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)->showAccessCameraReason()
+            else-> {
+                //Toast.makeText(this, "Need CAMERA permission", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun onGoAccessSettings(dialog : DialogInterface, which: Int) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+    }
+
+    private fun takeShot() = findViewById<ImageView>(R.id.imageview_photo)
+        .setImageResource(R.drawable.cat)
+
+    private fun selectImage() = takePicture.launch("image/*")
+
+    private val takePicture = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        //image->imageView.setImageURI(image)
+        uri->
+        if (uri != null) {
+            populateImage(uri)
+        }
     }
 }
